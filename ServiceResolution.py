1d466f191378a1bbbee5e18ae8ca3ee5b57f0e32
dhwz <dhwz@gmx.net>
2019-06-22 20:07:34 +0200

    converter ServiceResolution: added support for video codes (One)

--- ServiceResolution.py
+++ ServiceResolution.py
@@ -2,7 +2,7 @@
 #
 # ServiceResolution  - Converter
 #
-# Coded by dhwz (c) 2018
+# Coded by dhwz (c) 2018-2019
 # E-Mail: dhwz@gmx.net
 #
 # This plugin is open source but it is NOT free software.
@@ -19,10 +19,19 @@
 # you have to keep MY license and inform me about the modifications by mail.
 #
 
+try:
+	f = open("/proc/stb/info/model", "r")
+	model = ''.join(f.readlines()).strip()
+except:
+	model = ''
+
 from Components.Converter.Converter import Converter
 from enigma import iServiceInformation, iPlayableService
 from Components.Element import cached
 from os import path
+
+if model in ["one"]:
+	from enigma import CT_MPEG2, CT_H264, CT_MPEG1, CT_MPEG4_PART2, CT_VC1, CT_VC1_SIMPLE_MAIN, CT_H265, CT_DIVX311, CT_DIVX4, CT_SPARK, CT_VP6, CT_VP8, CT_VP9, CT_H263, CT_MJPEG, CT_REAL, CT_AVS, CT_UNKNOWN
 
 class ServiceResolution(Converter, object):
 	VIDEO_INFO = 0
@@ -31,11 +40,18 @@ class ServiceResolution(Converter, object):
 
 	def __init__(self, type):
 		Converter.__init__(self, type)
-		self.type, self.interesting_events = {
-				"VideoInfo": (self.VIDEO_INFO, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo,)),
-				"VideoInfoCodec": (self.VIDEO_INFOCODEC, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo,)),
-				"VideoCodec": (self.VIDEO_CODEC, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo,)),
-			}[type]
+		if model in ["one"]:
+			self.type, self.interesting_events = {
+					"VideoInfo": (self.VIDEO_INFO, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo,)),
+					"VideoInfoCodec": (self.VIDEO_INFOCODEC, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo, iPlayableService.evVideoTypeReady,)),
+					"VideoCodec": (self.VIDEO_CODEC, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo, iPlayableService.evVideoTypeReady,)),
+				}[type]
+		else:
+			self.type, self.interesting_events = {
+					"VideoInfo": (self.VIDEO_INFO, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo,)),
+					"VideoInfoCodec": (self.VIDEO_INFOCODEC, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo,)),
+					"VideoCodec": (self.VIDEO_CODEC, (iPlayableService.evVideoSizeChanged, iPlayableService.evVideoProgressiveChanged, iPlayableService.evVideoFramerateChanged, iPlayableService.evUpdatedInfo,)),
+				}[type]
 		self.need_wa = iPlayableService.evVideoSizeChanged in self.interesting_events
 
 	def reuse(self):
@@ -110,14 +126,22 @@ class ServiceResolution(Converter, object):
 				return "%s%s%s%s%s" % (xres, x, yres, p, frame_rate)
 		if self.type in [self.VIDEO_INFOCODEC, self.VIDEO_CODEC]:
 			codec = None
-			if path.exists("/proc/stb/vmpeg/0/codec"):
-				f = open("/proc/stb/vmpeg/0/codec", "r")
-				try:
-					codec = f.read().strip()
-					codec = codec.replace('H.264 (MPEG4 AVC)', 'H.264/AVC').replace('H.265 (HEVC)', 'H.265/HEVC')
-				except:
-					pass
-				f.close()
+			if model in ["one"]:
+				codec = info.getInfo(iServiceInformation.sVideoType)
+				codec = { CT_MPEG2 : "MPEG2", CT_H264 : "H.264/AVC", CT_MPEG1 : "MPEG1", CT_MPEG4_PART2 : "MPEG4",
+					CT_VC1 : "VC1", CT_VC1_SIMPLE_MAIN : "WMV3", CT_H265 : "H.265/HEVC", CT_DIVX311 : "DIVX3",
+					CT_DIVX4 : "DIVX4", CT_SPARK : "SPARK", CT_VP6 : "VP6", CT_VP8 : "VP8",
+					CT_VP9 : "VP9", CT_H263 : "H.263", CT_MJPEG : "MJPEG", CT_REAL : "RV",
+					CT_AVS : "AVS", CT_UNKNOWN : "UNKNOWN" }[codec]
+			else:
+				if path.exists("/proc/stb/vmpeg/0/codec"):
+					f = open("/proc/stb/vmpeg/0/codec", "r")
+					try:
+						codec = f.read().strip()
+						codec = codec.replace('H.264 (MPEG4 AVC)', 'H.264/AVC').replace('H.265 (HEVC)', 'H.265/HEVC')
+					except:
+						pass
+					f.close()
 			if self.type == self.VIDEO_INFOCODEC:
 				if codec and xres != "" :
 					return "%s%s%s%s%s [%s]" % (xres, x, yres, p, frame_rate, codec)
